1737f6fadd7ed098b38a6e8f242cdfa1b7f3a0d1
Sumanth Varma Dommaraju <svd260@nyu.edu>
2017-02-10 12:06:17 -0500

    spring jms - activeMq

--- src/main/java/com/twedittor/service/LikeDislikeListnerService.java
+++ src/main/java/com/twedittor/service/LikeDislikeListnerService.java
@@ -1,48 +1,12 @@
 package com.twedittor.service;
 
-import com.twedittor.domain.Quote;
-import com.twedittor.domain.User;
 import com.twedittor.jms.Like;
-import com.twedittor.repository.QuoteDao;
-import com.twedittor.repository.UserDao;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jms.annotation.JmsListener;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.jms.JMSException;
 
 /**
- * Created by sumanthdommaraju on 2/9/17.
+ * Created by sumanthdommaraju on 2/10/17.
  */
-
-@Component
-public class LikeDislikeListnerService {
-
-    private final QuoteDao quoteDao;
-    private final UserDao userDao;
-
-    @Autowired
-    public LikeDislikeListnerService(QuoteDao quoteDao, UserDao userDao) {
-        this.quoteDao = quoteDao;
-        this.userDao = userDao;
-    }
-
-
-
-    @JmsListener(containerFactory = "myFactory", destination = "likes.queue")
-    @Transactional
-    public void likeDisLike(Like like) throws JMSException {
-        Quote quote = quoteDao.findById(like.getQuoteId());
-        User user = userDao.findUserByUserId(like.getUserId());
-        if(like.getLike()) {
-            quote.getLikers().add(user);
-            //quote.getLiker().add(like.getUserId);
-        } else {
-            quote.getLikers().remove(user);
-            //quote.getLiker().remove(like.getUserId);
-
-        }
-        quoteDao.save(quote);
-    }
+public interface LikeDislikeListnerService {
+    void likeDisLike(Like like) throws JMSException;
 }
--- src/main/java/com/twedittor/service/LikeService.java
+++ src/main/java/com/twedittor/service/LikeService.java
@@ -1,35 +1,11 @@
 package com.twedittor.service;
 
-import com.twedittor.jms.Like;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.stereotype.Service;
-
 /**
- * Created by sumanthdommaraju on 2/9/17.
+ * Created by sumanthdommaraju on 2/10/17.
  */
-@Service
-public class LikeService {
+public interface LikeService {
 
-    @Value("${likes.destination}")
-    private String likesQueue;
+    void incrementLike(Long quoteId, String userId);
 
-    private final JmsTemplate jmsTemplate;
-
-    @Autowired
-    public LikeService(JmsTemplate jmsTemplate) {
-        this.jmsTemplate = jmsTemplate;
-    }
-
-    public void incrementLike(Long quoteId, String userId) {
-        Like like = new Like(quoteId, userId, true);
-        jmsTemplate.convertAndSend(likesQueue, like);
-    }
-
-    public void decrementLike(Long quoteId, String userId) {
-        Like like = new Like(quoteId, userId, false);
-        jmsTemplate.convertAndSend(likesQueue, like);
-    }
-
+    void decrementLike(Long quoteId, String userId);
 }
